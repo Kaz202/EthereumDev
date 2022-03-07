a154a473b8dd4e623deef973c19e4d9fc915af23
justkaz <kaz@justkaz.co.uk>
2022-03-07 18:19:27 +0000

    Work done 07/03/2022
    Work done 07/03/2022

--- blockchain-developer-bootcamp/test/Token.test.js
+++ blockchain-developer-bootcamp/test/Token.test.js
@@ -1,3 +1,6 @@
+import { tokens } from './helpers'
+
+
 const Token = artifacts.require('./Token')
 
 
@@ -5,11 +8,14 @@ require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('Token', (accounts) => {
+
+
+
+contract('Token', ([deployer, receiver]) => {
 	const name = 'DApp Token'
 	const symbol = 'DApp'
 	const decimals = '18'
-	const totalsupply = "1000000000000000000000000"
+	const totalSupply = tokens(1000000).toString()
 	let token
 	beforeEach(async () => {
 		token = await Token.new()
@@ -37,7 +43,48 @@ contract('Token', (accounts) => {
 
 		it('tracks the supply', async () => {
 			const result = await token.totalSupply()
-			result.toString().should.equal(totalsupply)
+			result.toString().should.equal(totalSupply.toString())
 		})
+
+
+		it('assigns the total supply to the deployer', async () => {
+			const result = await token.balanceOf(deployer)
+			result.toString().should.equal(totalSupply.toString()) 
+		})
+
+	})
+
+describe('sending tokens', () => {
+	let result
+	let amount
+
+	beforeEach(async () => {
+		amount = tokens(100)
+		result = await token.transfer(receiver, tokens(100), { from: deployer})
+	})
+
+		
+it('transfers token balances', async () => {
+        let balanceOf
+        balanceOf = await token.balanceOf(deployer)
+        balanceOf.toString().should.equal(tokens(999900).toString())
+        balanceOf = await token.balanceOf(receiver)
+        balanceOf.toString().should.equal(tokens(100).toString())
+      })
+
+
+it('emits a transfer event', async () => {
+	const log = result.logs[0]
+	log.event.should.eq('Transfer')
+	const event = log.arg
+	event.from.toString().should.equal(deployer, 'from is correct')
+	event.to.should.equal(receiver, 'to is correct')
+	event.value.toString().should.equal(amount.toString(), 'value is correct')
+
+})
+
+
+
+
 	})
 })
